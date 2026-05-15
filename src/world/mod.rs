d20c93c0fc9de70a11dd2932fda35920fd533000
isaac <isaacloff@gmail.com>
2026-05-15 19:00:04 +0100

    world

---
+++ src/world/mod.rs
@@ -0,0 +1,106 @@
+use std::{any::TypeId, collections::{HashMap, HashSet}};
+
+use hecs::{Access, PreparedQuery, PreparedQueryBorrow, Query};
+use parking_lot::RwLock;
+
+use crate::prelude::TrackedAccess;
+
+pub mod tracked_access;
+
+pub struct World {
+    world: hecs::World,
+    tracking: RwLock<HashMap<Vec<TypeId>, TrackedAccess>>
+}
+
+impl World {
+    pub fn check_query<Q: Query>(&self) -> Option<HashMap<Vec<TypeId>, Access>> {
+        let mut new_accesses = HashMap::new();
+
+        let tracking = self.tracking.read();
+
+        let compatible = !self.world.archetypes().any(|archetype| {
+            if let Some(access) = archetype.access::<Q>() {
+                let component_types = archetype.component_types().collect::<Vec<_>>();
+                let compatible = match (access, tracking.get(&component_types)) {
+                    (_, None) => true,
+                    (_, Some(TrackedAccess::Read(0))) => true,
+    
+                    (Access::Iterate, _) |
+                    (_, Some(TrackedAccess::Iterate)) => true,
+    
+                    (_, Some(TrackedAccess::Write)) |
+                    (Access::Write, _) => false,
+    
+                    (Access::Read, Some(TrackedAccess::Read(_))) => true,
+                };
+    
+                if compatible {
+                    new_accesses.insert(component_types, access);
+                }
+
+                !compatible
+            } else { true }
+        });
+
+        if compatible {
+            Some(new_accesses)
+        } else {
+            None
+        }
+    }
+
+    pub fn prepare_query<Q: Query>(&self) -> Option<(PreparedQuery<Q>, HashMap<Vec<TypeId>, Access>)> {
+        if let Some(new_accesses) = self.check_query::<Q>() {
+            let mut tracking = self.tracking.write();
+            let mut prepared_accesses = HashMap::new();
+
+            for (types, access) in new_accesses {
+                match tracking.get_mut(&types) {
+                    Some(current_access) => {
+                        match (access, current_access) {
+                            (Access::Iterate, _) => (),
+                            (Access::Read, TrackedAccess::Read(num)) => { 
+                                *num += 1;
+                                prepared_accesses.insert(types, access);
+                            },
+                            (_, TrackedAccess::Write) |
+                            (Access::Write, TrackedAccess::Read(_)) => unreachable!(),
+                            (access, current_access @ TrackedAccess::Iterate) => {
+                                let new_tracked_access = TrackedAccess::from(access);
+                                *current_access = new_tracked_access;
+                                prepared_accesses.insert(types, access);
+                            },
+                        }
+                    },
+                    None => {
+                        let new_tracked_access = TrackedAccess::from(access);
+                        tracking.insert(types.clone(), new_tracked_access);
+                        prepared_accesses.insert(types, access);
+                    },
+                }
+            }
+
+            Some((PreparedQuery::<Q>::new(), prepared_accesses))
+        } else { None }
+    }
+
+    pub fn finalise_query(&self, access_ids: &HashMap<Vec<TypeId>, Access>) {
+        let mut tracking = self.tracking.write();
+        for (types, access) in access_ids {
+            match tracking.get_mut(types) {
+                Some(current_access) => {
+                    match (access, current_access) {
+                        (Access::Iterate, _) => (),
+                        (Access::Read, TrackedAccess::Read(num)) => *num = if let Some(new_num) = num.checked_sub(1) { new_num } else { 0 },
+                        _ => unreachable!()
+                    }
+                },
+                None => todo!(),
+            }
+        }
+    }
+
+    pub fn do_query<'a, Q: Query>(&'a self, prepared_query: &'a mut PreparedQuery<Q>) -> PreparedQueryBorrow<'a, Q> {
+        prepared_query.query(&self.world)
+    }
+}
