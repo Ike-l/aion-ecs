39936c915a75a24fdb9b8e4dc64d7bf8127da65f
isaac <isaacloff@gmail.com>
2026-07-26 12:02:23 +0100

    Filled archetype access

--- src/world/archetype_registry/archetype_access/mod.rs
+++ src/world/archetype_registry/archetype_access/mod.rs
@@ -1,65 +1,89 @@
+use aion_program::prelude::StoredResourceTrait;
 use aion_state::prelude::Accessor;
 
 use crate::prelude::{Archetype, StoredArchetype};
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum ArchetypeAccess {
-    Shared(usize)
+    Shared(usize),
+    Replace,
 }
 
-pub enum ArchetypeAccessResult {}
+pub enum ArchetypeAccessResult<'a> {
+    Shared(&'a Archetype)
+}
 
 impl Accessor for ArchetypeAccess {
     type StoredValue = StoredArchetype;
 
     type Value = Archetype;
 
-    type AccessResult<'a> = ArchetypeAccessResult;
+    type AccessResult<'a> = ArchetypeAccessResult<'a>;
 
     fn accepts_incoming(&self, incoming_access: &Self) -> bool {
-        todo!()
+        match (self, incoming_access) {
+            (ArchetypeAccess::Shared(_), ArchetypeAccess::Shared(_)) => true,
+            (ArchetypeAccess::Replace, ArchetypeAccess::Replace) => true,
+            (ArchetypeAccess::Shared(0), ArchetypeAccess::Replace) => true,
+            (ArchetypeAccess::Shared(_), ArchetypeAccess::Replace) => false,
+            (ArchetypeAccess::Replace, ArchetypeAccess::Shared(_)) => true,
+        }
     }
 
     fn can_insert_resource(&self) -> bool {
-        todo!()
+        *self == Self::Replace
     }
 
     fn can_remove_resource(&self) -> bool {
-        todo!()
+        *self == Self::Replace
     }
 
     fn acquire<'a>(
         &self, 
         stored_value: &'a mut Self::StoredValue
     ) -> Self::AccessResult<'a> {
-        todo!()
+        match self {
+            ArchetypeAccess::Shared(0) => unreachable!(),
+            ArchetypeAccess::Shared(_) => ArchetypeAccessResult::Shared(stored_value.get()),
+            ArchetypeAccess::Replace => unreachable!(),
+        }
     }
 
     fn merge(
         &mut self,
         incoming_access: Self
     ) {
-        todo!()
+        match (self, incoming_access) {
+            (ArchetypeAccess::Shared(n), ArchetypeAccess::Shared(m)) => *n += m,
+            (m @ ArchetypeAccess::Shared(0), ArchetypeAccess::Replace) => *m = ArchetypeAccess::Replace,
+            (ArchetypeAccess::Shared(_), ArchetypeAccess::Replace) => unreachable!(),
+            (m @ ArchetypeAccess::Replace, n @ ArchetypeAccess::Shared(_)) => *m = n,
+            (ArchetypeAccess::Replace, ArchetypeAccess::Replace) => (),
+        }
     }
 
     fn release(
         &mut self,
         other: &Self
     ) {
-        todo!()
+        // probably could panic in other scenarios cause something probably went wrong?
+        match (self, other) {
+            (ArchetypeAccess::Shared(n), ArchetypeAccess::Shared(m)) => *n = n.saturating_sub(*m),
+            _ => ()
+        }
     }
 
     fn insert(
         &self,
         value: Self::Value
     ) -> Self::StoredValue {
-        todo!()
+        Self::StoredValue::new(value)
     }
 
     fn remove(
         &self,
         stored_value: Self::StoredValue
     ) -> Self::StoredValue {
-        todo!()
+        stored_value
     }
 }
--- src/world/archetype_registry/stored_archetype/mod.rs
+++ src/world/archetype_registry/stored_archetype/mod.rs
@@ -1,7 +1,27 @@
 use std::sync::Arc;
 
+use aion_program::prelude::StoredResourceTrait;
+
 use crate::prelude::Archetype;
 
 pub mod archetype;
 
-pub type StoredArchetype = Arc<Archetype>;
+pub struct StoredArchetype { 
+    inner: Arc<Archetype>
+}
+
+impl StoredResourceTrait for StoredArchetype {
+    type Resource = Archetype;
+
+    fn new(resource: Self::Resource) -> Self {
+        Self { inner: Arc::new(resource) }
+    }
+
+    fn get(&self) -> &Self::Resource {
+        &self.inner
+    }
+
+    fn get_mut(&mut self) -> &mut Self::Resource {
+        unreachable!()
+    }
+}
